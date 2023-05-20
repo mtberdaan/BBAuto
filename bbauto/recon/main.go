0270505bd25fd06fc9373ba98c4d5f4b22f699f8
Daan <daandenhoed@gmail.com>
2023-05-20 18:41:31 +0200

    logger

--- bbauto/recon/main.go
+++ bbauto/recon/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 	"recon/config"
 	"recon/resource"
 )
@@ -16,7 +17,24 @@ func (c *AppConfig) GetResourceCfgs() map[string]config.ResourceConfig {
 }
 
 func main() {
+	// init logging
+	LOG_FILE := "./logs/recon_log.log"
+	logFile, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
+
+	log.Println("...")
+	log.Println("...")
+	log.Println("...")
+
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
 	log.Print("INFO starting app...")
+
 	// initialize program configuration
 	c := &AppConfig{
 		ResourceCfgs: make(map[string]config.ResourceConfig),
@@ -27,7 +45,7 @@ func main() {
 
 	log.Println("INFO loading resource configs...")
 	// load resource configs
-	err := resource.LoadConfigs("./resource/configs", c)
+	err = resource.LoadConfigs("./resource/configs", c)
 	if err != nil {
 		log.Println("ERROR", err)
 		return
@@ -36,4 +54,6 @@ func main() {
 	for _, r := range c.GetResourceCfgs() {
 		log.Println("found resource:", r.ResourceName)
 	}
+
+	log.Print("INFO exit")
 }
